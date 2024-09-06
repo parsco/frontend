9749a642e2e3000df5a77c595364862230b47406
parsco <amir0078@ymail.com>
2024-09-06 20:49:55 +0330

    Update community-dialog.ts

--- src/onboarding/dialogs/community-dialog.ts
+++ src/onboarding/dialogs/community-dialog.ts
@@ -37,7 +37,7 @@ class DialogCommunity extends LitElement {
         <a
           target="_blank"
           rel="noreferrer noopener"
-          href="https://community.home-assistant.io/"
+          href="#"
         >
           <ha-list-item hasMeta graphic="icon">
             <img src="/static/icons/favicon-192x192.png" slot="graphic" />
@@ -72,7 +72,7 @@ class DialogCommunity extends LitElement {
         <a
           target="_blank"
           rel="noreferrer noopener"
-          href="https://x.com/home_assistant"
+          href="#"
         >
           <ha-list-item hasMeta graphic="icon">
             <img class="x" src="/static/images/logo_x.svg" slot="graphic" />
